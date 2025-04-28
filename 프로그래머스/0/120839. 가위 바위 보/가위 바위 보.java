9f892c0fdae3a1480399dd293d28c5a127227493
Park Ji Hyun <127265212+jihyuncoding@users.noreply.github.com>
2025-04-28 11:00:36 +0900

    [level 0] Title: 가위 바위 보, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String rsp) {
+        char[] arr = rsp.toCharArray();  
+
+        for (int i = 0; i < arr.length; i++) {
+            switch (arr[i]) {
+                case '2': arr[i] = '0'; break;  
+                case '0': arr[i] = '5'; break;  
+                case '5': arr[i] = '2'; break;  
+            }
+        }
+        return new String(arr);         
+    }
+}
