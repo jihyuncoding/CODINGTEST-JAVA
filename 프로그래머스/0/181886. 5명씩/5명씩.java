0735bc4f41d5c8e073f82af4ad838b164a9510b1
Park Ji Hyun <127265212+jihyuncoding@users.noreply.github.com>
2025-06-10 14:41:40 +0900

    [level 0] Title: 5명씩, Time: 0.05 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] names) {
+        ArrayList<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < names.length; i++) {
+            if (i == 0) { 
+                result.add(names[i]);
+            }
+            else if (i % 5 == 0) {
+                result.add(names[i]);
+            }
+        }
+        
+        return result.toArray(new String[0]);
+    }
+}
